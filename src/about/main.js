14d9082c2013290d1db47e142f66c9369f9a749f
oliviaengelhardt <olivia.engelhardt@me.com>
2019-11-02 19:45:20 +0100

    changes

--- src/about/main.js
+++ src/about/main.js
@@ -1,3 +1,14 @@
 import "../styles/main.styl";
 
-console.log("start");
+// const controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter" } });
+// const tl = new TimelineMax();
+// const intro = document.querySelector(".about-text");
+
+// intro.addEventListener("pageshow", onPageShow);
+// intro.addEventListener("pagehide", onPageHide);
+
+// function onPageShow() {
+    
+// }
+
+// tl.to(".about-text", 1, { alpha: 1, ease: Sine.easeOut }, 0)
